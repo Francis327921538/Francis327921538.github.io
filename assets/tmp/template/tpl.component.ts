a0d1e2f3c3188d4a4d304095c91f67b824804238
zzhang <327921538@qq.com>
2018-12-11 16:39:08 +0800

    1

---
+++ assets/tmp/template/tpl.component.ts
@@ -0,0 +1,17 @@
+// import {Component, OnInit} from '@angular/core';
+// import {NzMessageService} from 'ng-zorro-antd';
+// import {_HttpClient} from "@delon/theme";
+// import {CacheService} from "@delon/cache";
+//
+// @Component({
+//   selector: 'tpl',
+//   templateUrl: './tpl.component.html'
+// })
+//
+// export class SystemAuthorityComponent implements OnInit {
+//   constructor(public http: _HttpClient, private message: NzMessageService, public cacheService: CacheService) {
+//   }
+//
+//   ngOnInit(): void {
+//   }
+// }
